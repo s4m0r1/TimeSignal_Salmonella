cfb2c1ac9f4b2f0694a8e750b98e20003191d60b
s4m0r1 <bhms1270253@gn.iwasaki.ac.jp>
2020-03-06 14:51:14 +0900

    typescript移行準備

---
+++ index.ts
@@ -0,0 +1,6 @@
+const Discord = require("discord.js");
+const DiscordVoice = require("@discordjs/opus");
+const client = new Discord.Client();
+const cron = require("node-cron");
+require("dotenv").config();
+
