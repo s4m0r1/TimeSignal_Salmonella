3fbb147414621784451215887b58b2acd8464187
s4m0r1 <s4m0r1@ubuntu.ubuntu-domain>
2020-03-07 08:18:55 +0900

    scheduled done

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const Discord = __importStar(require("discord.js"));
 const dotenv = __importStar(require("dotenv"));
+const cron = __importStar(require("node-cron"));
 // setting
 const client = new Discord.Client();
 dotenv.config();
@@ -19,18 +20,22 @@ client.on('ready', () => {
     // tslint:disable-next-line:no-console
     console.log(`Logged in as ${client.user.tag}!`);
 });
-client.on('message', async (msg) => {
-    if (msg.content !== '/jiho')
+client.on("voiceStateUpdate", async (oldstate, newstate) => {
+    if (newstate.member === null)
         return;
-    if (msg.member === null)
+    if (newstate.member.voice.channel === null)
         return;
-    if (msg.member.voice.channel) {
-        const connect = await msg.member.voice.channel.join();
-        const dispatcher = connect.play('http://owncloud.s4m0r1.me/index.php/s/mB5RDpXdE9CaHey/download', { volume: 0.2 });
-        dispatcher.on('finish', () => connect.disconnect());
-    }
-    else {
-        msg.reply('ボイスチャンネルに入ってください☆');
+    if (newstate.member.user.bot)
+        return;
+    if (newstate.member.joinedAt) {
+        const connect = await newstate.member.voice.channel.join();
+        cron.schedule('0 0 0 * * *', () => {
+            const dispatcher = connect.play('http://owncloud.s4m0r1.me/index.php/s/mB5RDpXdE9CaHey/download', { volume: 0.2 });
+            dispatcher.on('finish', () => connect.disconnect());
+        }, {
+            scheduled: true,
+            timezone: "Asia/Tokyo"
+        });
     }
 });
 client.login(process.env.DISCORD_TOKEN);
