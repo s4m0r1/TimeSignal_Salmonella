df30df3573f494b604f4504d85a8f25d37d10ccb
s4m0r1 <s4m0r1@ubuntu.ubuntu-domain>
2020-03-06 17:43:05 +0900

    complete typescript play sound

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
 "use strict";
-var Discord = require("discord.js");
-var DiscordVoice = require("@discordjs/opus");
-var client = new Discord.Client();
-var cron = require("node-cron");
-require("dotenv").config();
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const Discord = __importStar(require("discord.js"));
+const dotenv = __importStar(require("dotenv"));
+// setting
+const client = new Discord.Client();
+dotenv.config();
+// Login message
+client.on("ready", () => {
+    // tslint:disable-next-line:no-console
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+client.on("message", async (msg) => {
+    if (msg.content === '/jiho') {
+        if (msg.member.voice.channel) {
+            // const connection: any = msg.member.voice.channel.join();
+            const connection = await msg.member.voice.channel.join();
+            const dispatcher = connection.play("http://owncloud.s4m0r1.me/index.php/s/mB5RDpXdE9CaHey/download");
+            dispatcher.on('finish', () => {
+                connection.disconnect();
+            });
+            // msg.member.voiceChannel.join()
+            // .then(connection => {
+            //     connection.play("http://owncloud.s4m0r1.me/index.php/s/mB5RDpXdE9CaHey/download");
+            // });
+            // const broadcast: any = client.voice.createBroadcast();
+            // broadcast.play("http://owncloud.s4m0r1.me/index.php/s/mB5RDpXdE9CaHey/download")
+            // connection.play(broadcast)
+        }
+        else {
+            msg.reply("ボイスチャンネルに入ってください☆");
+        }
+    }
+});
+// async function jiho(connection) {
+//     connection.play("http://owncloud.s4m0r1.me/index.php/s/mB5RDpXdE9CaHey/download");
+// }
+client.login(process.env.DISCORD_TOKEN);
--- index.ts
+++ index.ts
@@ -1,6 +1,32 @@
-const Discord = require("discord.js");
-const DiscordVoice = require("@discordjs/opus");
-const client = new Discord.Client();
-const cron = require("node-cron");
-require("dotenv").config();
+import * as Discord from 'discord.js'
+import * as DiscordVoice from '@discordjs/opus'
+import * as dotenv from 'dotenv'
+import * as cron from 'node-cron'
 
+// setting
+const client: any = new Discord.Client();
+dotenv.config();
+
+// Login message
+client.on("ready", () => {
+    // tslint:disable-next-line:no-console
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", async msg => {
+    if (msg.content === '/jiho') {
+        if (msg.member.voice.channel) {
+            const connection = await msg.member.voice.channel.join();
+            const dispatcher = connection.play("http://owncloud.s4m0r1.me/index.php/s/mB5RDpXdE9CaHey/download");
+            dispatcher.on('finish', () => {
+                connection.disconnect();
+            });
+        } else {
+            msg.reply("ボイスチャンネルに入ってください☆")
+        }
+    }
+})
+
+
+
+client.login(process.env.DISCORD_TOKEN);
