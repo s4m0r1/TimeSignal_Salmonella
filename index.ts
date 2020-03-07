a549551ab44b752ebf3bb56be5ef613c54c13d9c
s4m0r1 <bhms1270253@gn.iwasaki.ac.jp>
2020-03-07 18:13:56 +0900

    動くものが完成

--- index.ts
+++ index.ts
@@ -20,8 +20,10 @@ client.on("voiceStateUpdate", async (oldstate: Discord.VoiceState, newstate: Dis
     if(newstate.member.user.bot) return
     if(newstate.member.joinedAt) {
         const connect = await newstate.member.voice.channel.join()
-        cron.schedule('0 0 0 * * *', () => {
+        cron.schedule('0 0 * * * ', () => {
             const dispatcher = connect.play('http://owncloud.s4m0r1.me/index.php/s/mB5RDpXdE9CaHey/download', { volume: 0.2 })
+            //  tslint:disable-next-line:no-console
+            console.log('TimeSignal at 24')
             dispatcher.on('finish', () => connect.disconnect())
         }, {
             scheduled: true,
